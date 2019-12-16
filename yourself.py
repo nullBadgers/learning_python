d03fc484c360e6950a39a80c80e8fef88b283aa0
Mojo <somojo@users.noreply.github.com>
2019-12-16 20:45:08 +0000

    done

---
+++ yourself.py
@@ -0,0 +1,20 @@
+# Create a variable called name and set it equal to your name
+name = "The Dark Brown"
+
+# Create a variable called age and set it to your age in years 
+# Note: This must be an int, not a string!
+age = 666
+
+# Create a variable called place and set it equal to where you live
+place ="London"
+
+# Create a variable called output, and use {} symbols and the format() function to 
+# make it contain a string like the example text in the description
+
+love = "Python!"
+
+string = "Hello my name is {} and I am {} years old. I live in {} and I love {}"
+output = string.format(name, age, place, love)
+
+# Finally, use the print() function to print the output.
+print(output)
